f799a76c510949c4aaede99fd4821a6562e396b4
ELISA\acer <epsari56@gmail.com>
2016-12-07 21:32:48 +0800

    tes

--- Sistem Informasi Restoran/Form1.Designer.vb
+++ Sistem Informasi Restoran/Form1.Designer.vb
@@ -22,43 +22,130 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuMakananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MinumDessertToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PemesananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PembayaranToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TentangToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.InfoToolStripMenuItem, Me.TentangToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(610, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(821, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuMakananToolStripMenuItem, Me.MinumDessertToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'MenuMakananToolStripMenuItem
+        '
+        Me.MenuMakananToolStripMenuItem.Name = "MenuMakananToolStripMenuItem"
+        Me.MenuMakananToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.MenuMakananToolStripMenuItem.Text = "Makanan"
+        '
+        'MinumDessertToolStripMenuItem
+        '
+        Me.MinumDessertToolStripMenuItem.Name = "MinumDessertToolStripMenuItem"
+        Me.MinumDessertToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.MinumDessertToolStripMenuItem.Text = "Minum+Dessert"
+        '
+        'InfoToolStripMenuItem
+        '
+        Me.InfoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PemesananToolStripMenuItem, Me.PembayaranToolStripMenuItem})
+        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
+        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+        Me.InfoToolStripMenuItem.Text = "Info"
+        '
+        'PemesananToolStripMenuItem
+        '
+        Me.PemesananToolStripMenuItem.Name = "PemesananToolStripMenuItem"
+        Me.PemesananToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.PemesananToolStripMenuItem.Text = "Pemesanan"
+        '
+        'PembayaranToolStripMenuItem
+        '
+        Me.PembayaranToolStripMenuItem.Name = "PembayaranToolStripMenuItem"
+        Me.PembayaranToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.PembayaranToolStripMenuItem.Text = "Pembayaran"
+        '
+        'TentangToolStripMenuItem
+        '
+        Me.TentangToolStripMenuItem.Name = "TentangToolStripMenuItem"
+        Me.TentangToolStripMenuItem.Size = New System.Drawing.Size(63, 20)
+        Me.TentangToolStripMenuItem.Text = "Tentang"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(254, 87)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(284, 201)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Mistral", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(225, 24)
+        Me.Label1.Font = New System.Drawing.Font("Monotype Corsiva", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(12, 395)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(96, 33)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Resto Ke5"
+        Me.Label1.Size = New System.Drawing.Size(616, 14)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "  Ke5 Resto  ||   Jln. Samratulangi  ||   Samarinda   ||   Sistem Informasi Resto" & _
+            "ran  ||  Manajemen Informatika"
+        '
+        'Timer1
+        '
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(610, 417)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(821, 441)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.MenuStrip1)
+        Me.ForeColor = System.Drawing.Color.White
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Sistem Pemesanan"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MenuMakananToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MinumDessertToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PemesananToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PembayaranToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TentangToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
 
 End Class
--- Sistem Informasi Restoran/Form1.vb
+++ Sistem Informasi Restoran/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
 
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        If Label1.Left >= Me.Width Then
+            Label1.Left = -Label1.Width
+        Else
+            Label1.Left = Label1 + 100
+        End If
+    End Sub
+
+
 End Class
